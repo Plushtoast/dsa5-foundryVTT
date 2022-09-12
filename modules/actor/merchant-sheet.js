8cd9ed663717e1c2ddc736f73fa319c18683885d
Jan Weigmann <janweigmann@web.de>
2022-09-12 11:37:52 +0200

    bun up

--- modules/actor/merchant-sheet.js
+++ modules/actor/merchant-sheet.js
@@ -1,6 +1,4 @@
 import ActorSheetdsa5NPC from "./npc-sheet.js";
 import { MerchantSheetMixin } from "./merchantmixin.js";
 
-export default class MerchantSheetDSA5 extends MerchantSheetMixin(ActorSheetdsa5NPC) {
-
-}
+export default class MerchantSheetDSA5 extends MerchantSheetMixin(ActorSheetdsa5NPC) {}
