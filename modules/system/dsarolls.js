26af584cfbf0622f7309219d32fe6ad81822645e
Jan Weigmann <janweigmann@web.de>
2022-09-23 11:07:42 +0200

    workaround for odd

--- modules/system/dsarolls.js
+++ modules/system/dsarolls.js
@@ -7,7 +7,8 @@ export default function() {
             let curindex = 0
             
             for (let term of this.terms) {
-                const isDie = term instanceof Die || term.class == "Die"
+                const isDie = term instanceof DiceTerm || term.class == "DiceTerm"
+                const isOperator = term instanceof OperatorTerm
                 if (isDie || term.faces) {
                     if (term.results[index - curindex]) {
                         let oldVal = term.results[index - curindex].result
@@ -15,10 +16,14 @@ export default function() {
                         results.push(oldVal)
                     }
 
-                    //Todo this should not be necessary
+                    //Todo this should not be necessary once toJSON of Roll class fixed (toJSON needs to have terms.toJSONed)
                     if(!isDie) term.total = term.results.reduce((prev, cur) => { return prev + cur.result }, 0)
 
                     curindex += term.results.length
+                } 
+                //Todo this should not be necessary once toJSON of Roll class fixed
+                else if(!isOperator && (term.class == "OperatorTerm" || term.operator)) {
+                    term.total = term.operator
                 }
             }
             results.push(0)
