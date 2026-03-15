bd2197d0a567ce93274bd2128bcb287c7c47ac7a
Jan <janweigmann@web.de>
2026-03-15 12:15:36 +0100

    7.5.0 bugfix and sale

--- modules/system/calendar/personaedramatis.js
+++ modules/system/calendar/personaedramatis.js
@@ -294,14 +294,16 @@ export class PersonaeDramatis {
 
         this.close();
 
-        const activatedJournals = game.settings.get('dsa5', DSAPersonaEntry.SETTING_NAME)?.activated || [];
+        const settings = game.settings.get('dsa5', DSAPersonaEntry.SETTING_NAME) || { activated: [] };
+        settings.activated ||= [];
+        const activatedJournals = settings.activated;
         if (activatedJournals.length === 0) {
             const newJournal = await JournalEntry.create({
                 name: localize("PERSONAE.ImportantPersons"), pages: [{
                     name: localize("PERSONAE.ImportantPersons"), type: "dsapersonaedramatis",
                 }]
             });
-            settings.activated.push({ uuid: newJournal.id, name: newJournal.name });
+            settings.activated.push({ uuid: newJournal.uuid, name: newJournal.name });
             await game.settings.set('dsa5', DSAPersonaEntry.SETTING_NAME, settings);
             newJournal.sheet.render(true);
         } else {
