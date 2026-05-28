239626371a229f0ce9608a05a405045549c1cccf
Jan <janweigmann@web.de>
2026-05-28 19:46:29 +0200

    Animal hotbar does not recognize spells Fixes #2461

--- modules/actor/companions/companion-hotbar.js
+++ modules/actor/companions/companion-hotbar.js
@@ -3,6 +3,23 @@ const ICON_SUMMON = 'fa-bell';
 const POSITION_STYLE = 'left:calc(50% + 40px);top:1px;';
 
 export default class CompanionHotbar {
+  static ROLLABLE_CATEGORIES = {
+    skill: { label: 'TYPES.Item.skill', setup: 'setupSkill' },
+    spell: { label: 'TYPES.Item.spell', setup: 'setupSpell' },
+    ritual: { label: 'TYPES.Item.ritual', setup: 'setupSpell' },
+    liturgy: { label: 'TYPES.Item.liturgy', setup: 'setupSpell' },
+    ceremony: { label: 'TYPES.Item.ceremony', setup: 'setupSpell' },
+  };
+
+  static async executeHotbarRoll(actor, item, tokenId) {
+    const config = this.ROLLABLE_CATEGORIES[item.type];
+    if (!config) return false;
+
+    const setupData = await actor[config.setup](item, {}, tokenId);
+    await actor.basicTest(setupData);
+    return true;
+  }
+
   /**
    * Resolve the companion relationship for the current hotbar actor.
    * @param {Actor} actor - The hotbar's active actor
